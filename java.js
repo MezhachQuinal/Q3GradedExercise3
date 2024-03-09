518a2be700cf57dcd0f25754d8fbf6d7cba2be3a
MezhachQuinal <143673443+MezhachQuinal@users.noreply.github.com>
2024-03-09 09:19:33 +0800

    Add files via upload

---
+++ java.js
@@ -0,0 +1,26 @@
+function generate(){
+    var num = Math.floor(Math.random() * 1001);
+    var thousands0 = Math.floor(num/1000);
+    var thousands = thousands0 * 1000;
+    var hundreds0 = Math.floor(num/100);
+    var hundreds = hundreds0 * 100;
+    var tens0 = Math.floor((num - hundreds)/10);
+    var tens = tens0 * 10;
+    var ones = Math.floor(num - hundreds - tens);
+
+    document.getElementById("number").innerHTML = "Randomly generated number: " + num;
+    if(num === 1000){
+        document.getElementById("downbreak").innerText = "Breakdown: " + thousands + ", " + hundreds + ", " + tens + ", " + ones;
+    } else if(num < 1000 && num >= 100){
+        document.getElementById("downbreak").innerText = "Breakdown: " + hundreds + ", " + tens + ", " + ones;
+    } else if(num < 100 && num >= 10){
+        document.getElementById("downbreak").innerText = "Breakdown: " + tens + ", " + ones;
+    } else if(num < 10 && num >= 1){
+        document.getElementById("downbreak").innerText = "Breakdown: " + ones;
+    }
+    var generation = "Generated on: ";
+    var date = new Date();
+    var formattedDate = date.toLocaleDateString('en', { timeZoneName: 'short'});
+    var sentenceofdate = generation.concat(innerHTML = formattedDate, ".");
+    document.getElementById("date").innerHTML = sentenceofdate;
+}
